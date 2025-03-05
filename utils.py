a83521a1bbd804f851ee17a2fecc1d058e7ac231
Parithimaal <balajiadithya2000@gmail.com>
2025-03-04 21:18:46 -0500

    Model 1 done

--- utils.py
+++ utils.py
@@ -7,7 +7,8 @@ class Utils:
     def fetch_data(tickers, labels, start_date, end_date):
         ticker_label = dict()
         all_prices_df = yf.download(tickers, start=start_date, end=end_date)['Close']
+        all_prices_df = all_prices_df.rename(columns=dict(zip(tickers, labels)))
         all_prices_dict = {}
-        for ticker, label in zip(tickers, labels):
-            all_prices_dict[label] = all_prices_df[[ticker]]
-        return all_prices_dict
+        for col in all_prices_df.columns:
+            all_prices_dict[col] = all_prices_df[[col]]
+        return all_prices_dict, all_prices_df
